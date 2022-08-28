f8abe03c9cf5e4de37b359245982cdfa142fe30c
Danilo Sousa <danilo.souza@sergipetec.org.br>
2022-08-28 01:02:20 -0300

    Adding CRUD with tests in application

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -2,56 +2,112 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Models;
+using Shop.Data;
+using System;
+using Microsoft.EntityFrameworkCore;
 
 [Route("categories")]
-public class CategoryController : ControllerBase 
+public class CategoryController : ControllerBase
 {
+    private DataContext _context;
+
+    public CategoryController(DataContext context)
+    {
+        _context = context;
+    }
 
     [HttpGet]
     [Route("")]
     public async Task<ActionResult<List<Category>>> Get()
     {
-        return new List<Category>();
+        var categories = await _context.Categories.AsNoTracking<Category>().ToListAsync();
+        return Ok(categories);
     }
 
 
     [HttpGet]
     [Route("{id:int}")]
-    public  async Task<ActionResult<Category>> GetById(int id)
+    public async Task<ActionResult<Category>> GetById(int id)
     {
-        return new Category();
+        var category = await _context.Categories.FirstOrDefaultAsync( category => category.Id == id);
+        if(category == null)
+        return NotFound(new { message = "Não encontramos essa categoria"});
+
+        try
+        {
+            return Ok(category);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message});
+        }
     }
 
     [HttpPost]
     [Route("")]
-    public async Task<ActionResult<List<Category>>> Post([FromBody]Category model)
+    public async Task<ActionResult<List<Category>>> Post([FromBody] Category model)
     {
-        if(!ModelState.IsValid)
+        if (!ModelState.IsValid)
             return BadRequest(ModelState);
 
-        return Ok();
+        try
+        {
+            _context.Categories.Add(model);
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch
+        {
+            return BadRequest(new { message = "Não foi possível criar esta categoria"});
+        }
     }
 
     [HttpPut]
     [Route("{id:int}")]
-    public async Task<ActionResult<List<Category>>> Put(int id, [FromBody]Category model)
+    public async Task<ActionResult<List<Category>>> Put(int id, [FromBody] Category model)
     {
         // Verifica se o ID ifnromado é o mesmo do modelo
-       if(model.Id != id)
-       return NotFound(new { menssage = "Categoria não encontrada"});
+        if (model.Id != id)
+            return NotFound(new { menssage = "Categoria não encontrada" });
 
-       // Verifica se os dados não são inválidos
-       if(!ModelState.IsValid)
-       return BadRequest(ModelState);
+        // Verifica se os dados não são inválidos
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
 
-       return Ok(model);
+        try
+        {
+            _context.Entry<Category>(model).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok(model);
+        }
+        catch
+        {
+            return BadRequest(new { message = "Não foi possível atualizar esta categoria"});
+        }
     }
 
     [HttpDelete]
     [Route("{id:int}")]
     public async Task<ActionResult<List<Category>>> Delete(int id)
     {
-        return Ok();
+        // Verifica se os dados não são inválidos
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        var category = await _context.Categories.FirstOrDefaultAsync( category => category.Id == id);
+        if(category == null)
+        return NotFound(new { message = "Não encontramos essa categoria"});
+
+        try
+        {
+            _context.Categories.Remove(category);
+            await _context.SaveChangesAsync();
+            return Ok(new { message = "Categoria removida com sucesso!"});
+        }
+        catch
+        {
+            return BadRequest(new { message = "Não foi possível atualizar esta categoria"});
+        }
     }
-    
+
 }
