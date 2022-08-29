bf649de4208dc54c3ec7d52103c53bdc2b892d1d
Danilo Sousa <danilo.souza@sergipetec.org.br>
2022-08-28 23:55:57 -0300

    Adding structur  user to can login in application

---
+++ Settings.cs
@@ -0,0 +1,7 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        public static string Secret = "4054bd4c-39e4-4030-b7f9-9b5310cb900c";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Shop.Data;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Shop
 {
@@ -21,6 +24,21 @@ namespace Shop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication(user => {
+                user.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                user.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(user => {
+                user.RequireHttpsMetadata = false;
+                user.SaveToken = true;
+                user.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
             // Variável de ambiente para teste da aplicação com dados em memória!
             // services.AddDbContext<DataContext>(options => options.UseInMemoryDatabase("Database"));
             services.AddDbContext<DataContext>(options => options.UseSqlServer(Configuration.GetConnectionString("connectionString")));
@@ -39,6 +57,7 @@ namespace Shop
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
